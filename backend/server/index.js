63ff6a2036b6a3249b85c54631ba0b426fb4580c
Barak Cohen <barakc@semperis.com>
2021-06-07 09:38:19 +0300

    Return to last code nodeJS

--- backend/server/index.js
+++ backend/server/index.js
@@ -8,56 +8,10 @@ require('dotenv').config();
 const app = express();
 
 
-const msal = require('@azure/msal-node');
+
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Msal Node Auth Code Sample app listening on port ${port}!`));
+app.listen(port, () => console.log(`Server is start on port ${port}`) )
 
-const config = {
-    auth: {
-        clientId: process.env.clientId,
-        authority: process.env.authority,
-        clientSecret: process.env.clientSecret
-    },
-    system: {
-        loggerOptions: {
-            loggerCallback(loglevel, message, containsPii) {
-                console.log(message);
-            },
-            piiLoggingEnabled: false,
-            logLevel: msal.LogLevel.Verbose,
-        }
-    }
-};
-
-const cca = new msal.ConfidentialClientApplication(config);
-
-app.get('/', (req, res) => {
-    const authCodeUrlParameters = {
-        scopes: ["user.read"],
-        redirectUri: "http://localhost:5000/redirect",
-    };
-
-    // get url to sign user in and consent to scopes needed for application
-    cca.getAuthCodeUrl(authCodeUrlParameters).then((response) => {
-        res.redirect(response);
-    }).catch((error) => console.log(JSON.stringify(error)));
-});
-
-app.get('/redirect', (req, res) => {
-    const tokenRequest = {
-        code: req.query.code,
-        scopes: ["user.read"],
-        redirectUri: "http://localhost:5000/redirect",
-    };
-
-    cca.acquireTokenByCode(tokenRequest).then((response) => {
-        console.log("\nResponse: \n:", response);
-        res.sendStatus(200);
-    }).catch((error) => {
-        console.log(error);
-        res.status(500).send(error);
-    });
-});
 // mongoose.connect(process.env.PG_URI, {
 //     dbName:'ParkingDB',
 //     useNewUrlParser: true,
@@ -72,5 +26,3 @@ app.use(cors());
 const parkings = require('./routes/parkings');
 
 app.use('/api/parkings', parkings);
-
-
